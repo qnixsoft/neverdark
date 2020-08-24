6f03f273ce04b70c5afe6bf526ded11be9cae956
Q <q@qnixsoft.com>
2020-08-24 10:57:01 +0100

    I/O tty cli

--- client/main.c
+++ client/main.c
@@ -3,6 +3,10 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <sys/select.h>
+#include <errno.h>
+#include <string.h>
+#include <curses.h>
 #include "tty.h"
 
 #define FIXEDSTR(a) a, (sizeof(a) + 1)
@@ -10,10 +14,14 @@
 int
 main(int argc, char *argv[], char *envp[])
 {
-	int sockfd, connfd; 
-	struct sockaddr_in servaddr, cli; 
+	int sockfd, n; 
+	struct sockaddr_in servaddr; 
 	char inbuf[2 * BUFSIZ];
 	char outbuf[4 * BUFSIZ];
+	char username[64];
+	char password[64];
+	struct timeval tv;
+	fd_set rd;
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
 	if (sockfd < 0) {
@@ -30,15 +38,55 @@ main(int argc, char *argv[], char *envp[])
 		return -1;
 	} 
 
-	write(sockfd, FIXEDSTR("auth One qovmjbl")); 
+	fprintf(stderr, "Username: ");
+	scanf("%s", username);
+	fprintf(stderr, "Password: ");
+	initscr();
+	noecho();
+	scanf("%s", password);
+	echo();
+	endwin();
+
+	setvbuf(stdin, NULL, _IOLBF, 0);
+	setvbuf(stdout, NULL, _IOLBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+
+	n = snprintf(inbuf, sizeof(inbuf), "auth %s %s", username, password);
+	write(sockfd, inbuf, n + 2); 
 
 	for (;;) {
-		ssize_t ret = read(sockfd, inbuf, sizeof(inbuf));
-		if (ret <= 0)
-			break;
-		inbuf[ret] = '\0';
-		tty_proc(outbuf, inbuf);
-		printf("%s", outbuf);
+		FD_ZERO(&rd);
+		FD_SET(0, &rd);
+		FD_SET(sockfd, &rd);
+		tv.tv_sec = 120;
+		tv.tv_usec = 0;
+		n = select(sockfd + 1, &rd, 0, 0, &tv);
+		if (n < 0) {
+			if (errno == EINTR)
+				continue;
+			perror("select");
+		} else if (n == 0) {
+			perror("0 on select");
+		} else if (FD_ISSET(sockfd, &rd)) {
+			ssize_t ret = read(sockfd, inbuf, sizeof(inbuf));
+			if (ret < 0) {
+				if (errno == EINTR)
+					continue;
+				perror("read");
+				break;
+			}
+			else if (ret == 0)
+				continue;
+			inbuf[ret] = '\0';
+			tty_proc(outbuf, inbuf);
+			printf("%s", outbuf);
+		} else if (FD_ISSET(0, &rd)) {
+			char *ptr = fgets(inbuf, sizeof(inbuf), stdin);
+			if (ptr == NULL)
+				perror("fgets");
+			write(sockfd, ptr, strlen(ptr) + 1);
+		}
+
 	}
 
 	close(sockfd);
